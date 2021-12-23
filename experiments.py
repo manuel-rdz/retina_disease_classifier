05b37256652c4da497911af8dc4d3b58015909a3
manuel-rdz <manuel.alj.rdz@gmail.com>
2021-12-23 19:46:17 +0400

    Added support for custom img_size in evaluation and distilled models.

--- experiments.py
+++ experiments.py
@@ -1,3 +1,9 @@
-import timm
+#import timm
 
-print(timm.list_models('xcit*'))
+#print(timm.list_models('*it*', pretrained=True))
+
+nums = [-1,1,2,3,4,5,6,7]
+
+
+if (-1 in nums) and (sorted(nums)[1:]) in {list(range(2, len(nums))), list(range(1, len(nums)-1))}:
+    pass
